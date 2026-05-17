d3eda19f1756cad15416f9753fb9fb259451ce51
Shrey <liveprosperdie7@gmail.com>
2026-05-17 17:43:29 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-file_types=
+file_types={
+    "images":[".jpg",".png",".gif",".svg",".webp"],
+    "videos":[".mp4",".mkv",".mov",".avi"],
+    "audio":[".mp3",".wav",".flac"],
+    "documents":[".pdf",".docx",".txt",".xlsx",".pptx"],
+    "code":[".py",".js",".html",".css",".java",".c"],
+    "archives":[".zip",".rar",".tar",".gz"],
+    "others":[]
+}
---
+++ organizer.py
@@ -0,0 +1,6 @@
+import config
+
+
+def get_folder(ext):
+    for folder in config.file_types:
+        
