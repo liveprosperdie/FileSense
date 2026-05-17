2dee80decc254edbfcbc7ce5b140d692b910d79e
Shrey <liveprosperdie7@gmail.com>
2026-05-17 20:30:56 +0530

    Adding to organise function

--- organizer.py
+++ organizer.py
@@ -1,4 +1,5 @@
 import config
+import os
 
 
 def get_folder(ext):
@@ -7,5 +8,15 @@ def get_folder(ext):
             return folder
     return "others"
 
-def organise(path):
-    
+def organise(folder_path):
+    try:
+        file_entries=[f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path,f))]
+    except FileNotFoundError:
+        print("The specific folder doesn't exist.")
+    except PermissionError:
+        print("You don't have permission to access this directory.")
+    except NotADirectoryError:
+        print("The path provided points to a file, not a folder")
+    else:
+        for file in file_entries:
+            file_path=os.path.join(folder_path,file)
