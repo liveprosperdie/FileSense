498cb1ea7071b8becc31e69d5f1136015f220af8
Shrey <liveprosperdie7@gmail.com>
2026-05-18 13:15:49 +0530

    watcher complete

--- watcher.py
+++ watcher.py
@@ -1,5 +1,6 @@
-import organizer
+import time
 import logger
+import organizer
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -12,5 +13,19 @@ class Folder_Handler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory:
             organizer.organise(self.folder_path)
+            logger.log_action(f"{event.src_path} created")
 
 
+def observe(folder_path):
+    event_handler=Folder_Handler(folder_path)
+    observer=Observer()
+    observer.schedule(event_handler, path=folder_path, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+    
