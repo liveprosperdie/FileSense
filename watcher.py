5a6af49dffd1325c9d133c3aa54fbb62680cfd9d
Shrey <liveprosperdie7@gmail.com>
2026-05-18 12:59:24 +0530

    setting handler

--- watcher.py
+++ watcher.py
@@ -1,1 +1,16 @@
-import watchdog
+import organizer
+import logger
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Folder_Handler(FileSystemEventHandler):
+    
+    def __init__(self,folder_path):
+        self.folder_path=folder_path
+
+    def on_created(self, event):
+        if not event.is_directory:
+            organizer.organise(self.folder_path)
+
+
