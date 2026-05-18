dce25daee93fc6a31266e40d56a4b676d6e8e287
Shrey <liveprosperdie7@gmail.com>
2026-05-18 13:39:26 +0530

    complete

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import watcher
+import argparse
 import organizer
 
 
 def main():
+    parser=argparse.ArgumentParser(description="File Organiser running: 1- manual 2- watch")
+    parser.add_argument("--mode", default="", help="To decide the mode of operations 1-manual (for running once manually) 2-watch (for running in back for a dir until stopped)")
+    args=parser.parse_args()
     folder_path=input("Enter the folder to organise: ")
-    organizer.organise(folder_path)
+    if args.mode == "manual":
+        organizer.organise(folder_path)
+    elif args.mode == "watch":
+        watcher.observe(folder_path)
+    else:
+        print("Wrong args passed please check help")
 
 if __name__=="__main__":
     main()
